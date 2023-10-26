60c68ab9535e88f3f4fbcc6dbe93cc000d109d19
Marc raphael <marcraphael979@gmail.com>
2023-10-26 11:40:23 +0100

    dropp all database

--- db/migrate/20230823150411_add_title_column_to_post.rb
+++
@@ -1,5 +0,0 @@
-class AddTitleColumnToPost < ActiveRecord::Migration[7.0]
-  def change
-    add_column :posts, :Title, :string
-  end
-end
--- db/migrate/20230908093119_add_unconfirmed_email_to_user.rb
+++
@@ -1,6 +0,0 @@
-class AddUnconfirmedEmailToUser < ActiveRecord::Migration[7.0]
-  change_table :users do |t|
-    # Confirmable
-    t.string   :unconfirmed_email
-  end
-end
