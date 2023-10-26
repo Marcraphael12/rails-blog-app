1b3c09a50e5c137b34be84903e78329e080dc151
Marc raphael <marcraphael979@gmail.com>
2023-10-26 13:20:36 +0100

    Update migartions
    - Create databese
    - Write schema with migration

--- db/migrate/20231026110112_create_comments.rb
+++ db/migrate/20231026110112_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
-    create_table :coments do |t|
-      t.references :author, foreign_key: { to_table: :users }
+    create_table :comments do |t|
+      t.references :user, foreign_key: { to_table: :users }
       t.references :post, foreign_key: true
       t.text :text
 
