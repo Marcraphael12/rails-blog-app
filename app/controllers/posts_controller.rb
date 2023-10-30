2db4d2468941b1cc1963f8e1c08426a67934706f
Marc raphael <marcraphael979@gmail.com>
2023-10-30 17:30:23 +0100

    Update actions in controllers
    - Update Index action for posts
    - Add docs

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,16 @@
-class PostsController < ActonController
-  def index; end
+class PostsController < ApplicationController
+  def index
+    # Find a specific user based on the `id` parameter passed
+    # in the request. It assigns the found user to the `@users` instance variable, which can then be
+    # used in the view to display the details of the user.
+    @users = User.find(params[:author_id])
 
-  def show; end
+    @posts = @users.posts
+  end
+
+  def show
+    # Find a specific post based on the `id` parameter passed
+    # in the request. It assigns the found post to the `@posts` instance variable, which can then be
+    # used in the view to display the details of the post.
+  end
 end
