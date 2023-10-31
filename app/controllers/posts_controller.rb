cfd67dc9aa1fc1bead9463cbd4d382c86af9209c
Marc raphael <marcraphael979@gmail.com>
2023-10-31 11:33:40 +0100

    Update controllers
    - Add comments and likes controllers
    - Create actions for comments and likes controllers
    -  Add create action to posts controller
    - Remane variable in show action for user controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,8 @@ class PostsController < ApplicationController
     # Find a specific user based on the `id` parameter passed
     # in the request. It assigns the found user to the `@users` instance variable, which can then be
     # used in the view to display the details of the user.
-    @users = User.find(params[:user_id])
+    @users = User.find(params[:author_id])
+    @user = User.find(params[:author_id])
 
     @posts = @users.posts
   end
@@ -13,5 +14,27 @@ class PostsController < ApplicationController
     # in the request. It assigns the found post to the `@posts` instance variable, which can then be
     # used in the view to display the details of the post.
     @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  ##
+  # creates a new post associated with a user and redirects to the user's page
+  # if successful, or renders the new post form with an error message if unsuccessful.
+  def create
+    @author = User.find(params[:author_id])
+    @post = @author.posts.new(post_params)
+
+    if @post.save
+      redirect_to user_path(id: @post.author_id)
+    else
+      render :new, alert: 'An error has occurred while creating the post'
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
