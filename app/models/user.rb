a6320eba1c63f06f8e58fe96e7ffb4b39cc5cd78
Marc raphael <marcraphael979@gmail.com>
2023-11-09 17:20:29 +0100

    Update models
    - Remove useless validations
    - Add devises rules for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,28 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+        #  :confirmable
   has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
 
-  def recents_posts
-    posts.order(created_at: :desc).limit(3)
+  # validating that the `Name` attribute of the `User`
+  # model is present, meaning it cannot be blank or nil.
+  validates :name, presence: true
+
+  validates :post_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  def recent_posts(limit = 3)
+    # retrieve the most recent posts for a user.
+    posts.order(created_at: :desc).limit(limit)
   end
 
-  def all_posts
-    posts.order(created_at: :desc)
+  def update_posts_counter
+    # updating the `posts_counter` attribute of the
+    # `User` model with the count of the user's posts. This is useful for keeping track of the number
+    # of posts a user has.
+    update(post_counter: posts.count)
   end
 end
