8abf3ca9d2393458a83bb497e595b8c9445c7751
Marc raphael <marcraphael979@gmail.com>
2023-11-09 17:21:25 +0100

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,21 @@
 class ApplicationController < ActionController::Base
-  def current_user
-    @current_user = User.find(1)
-  end
+  #  A method call that is used to protect against
+  # Cross-Site Request Forgery (CSRF) attacks in Rails applications.
+  protect_from_forgery with: :exception
+  
+  before_action :authenticate_user!
 
-  helper_method :current_user
+  # setting up a
+  # callback that will call the `update_allowed_parameters` method before executing any action in the
+  # controller, but only if the current controller is a `devise_controller`.
+  before_action :update_allowed_parameters, if: :devise_controller?
+
+  def update_allowed_parameters
+    # configuring the permitted parameters for the sign-up action in the Devise gem.
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
+
+    # configuring the permitted parameters for the account update
+    # action in the Devise gem.
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
+  end
 end
