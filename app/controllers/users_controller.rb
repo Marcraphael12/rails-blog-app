75c84d9ca7dc840aa8f3967bac4ea6f5c3cbe247
Marc raphael <marcraphael979@gmail.com>
2023-11-09 17:24:00 +0100

    Add docs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,14 @@
 class UsersController < ApplicationController
   def index
+    # retrieving all the records from the `User` model and assigning them to the
+    # `@users` instance variable. This allows the retrieved users to be accessed and used in the view.
     @users = User.all
   end
 
   def show
+    # Find a specific user based on the `id` parameter passed
+    # in the request. It assigns the found user to the `@users` instance variable, which can then be
+    # used in the view to display the details of the user.
     @user = User.find(params[:id])
-    @users = User.all
-    @current_user = current_user
   end
 end
