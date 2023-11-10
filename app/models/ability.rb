17eb2e1ced8d767b3bb106afe3ca12ddc2ec18ac
Marc raphael <marcraphael979@gmail.com>
2023-11-10 15:03:49 +0100

    Update ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,19 +3,16 @@ class Ability
 
   def initialize(user)
     # Define abilities for the passed in user
-    if user.admin?
-      can :manage, :all # Admin can manage all resources
+    if user.role == 'admin'
+      can :manage, :all
     else
-      can :read, :all
-      can :create, Post, author_id: user.id
-      can :create, Comment, author_id: user.id
-      can :create, Like
-      can :destroy, Post do |post|
-        post.author_id == user.id
-      end
-      can :destroy, Comment do |comment|
-        comment.author_id == user.id
-      end
+      can :read, Post # All users can read posts
+      can :create, Post if user.persisted? # Logged-in users can create posts
+      can :update, Post, author_id: user.id # Users can update their own posts
+      can :destroy, Post, author_id: user.id # Users can delete their own posts
+
+      can :create, Comment if user.persisted? # Logged-in users can create comments
+      can :destroy, Comment, author_id: user.id # Users can delete their own comments
     end
   end
 end
